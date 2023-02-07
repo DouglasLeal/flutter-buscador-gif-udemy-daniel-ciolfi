da99374f3ea3b454c84e65b2ea163972dd7fa748
Douglas Leal <douglas.leal@live.com>
2023-02-07 17:39:02 -0300

    Criado appbar e textfield

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     super.initState();
@@ -19,6 +18,40 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: _appbar(),
+      body: _body(),
+    );
+  }
+
+  _appbar() {
+    return AppBar(
+      title: Image.network(
+          "https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif"),
+      centerTitle: true,
+      backgroundColor: Colors.black,
+    );
+  }
+
+  _body() {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: Column(children: const [
+        TextField(
+          decoration: InputDecoration(
+            labelText: "Procurar...",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+            border: OutlineInputBorder(),
+          ),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+          ),
+        ),
+      ]),
+    );
   }
 }
