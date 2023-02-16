e66d322d629508bc0360299b0c900d9b6f32810a
Douglas Leal <douglas.leal@live.com>
2023-02-15 23:26:21 -0300

    readme editado

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         inputDecorationTheme: const InputDecorationTheme(
           focusedBorder: OutlineInputBorder(
